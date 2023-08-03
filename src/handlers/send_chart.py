97871de858780a3f58497d334f67a69201fd1695
Constantine <cborovskoy@gmail.com>
2023-08-04 01:29:27 +0600

    Update send_chart.py

--- src/handlers/send_chart.py
+++ src/handlers/send_chart.py
@@ -1,16 +1,18 @@
 from aiogram import Router, Bot
 from aiogram.types import Message, CallbackQuery, InputMediaPhoto, BufferedInputFile
 
-from src.config import LABEL_1M, LABEL_TIME, LABEL_15M, LABEL_1H, CT_LINE, CT_CANDLESTICK, load_config
+from src.config import load_config, LABEL_1H, LABEL_15M, LABEL_1M
 from src.keyboards.main_kb import get_main_kb
 from src.service.charts import make_chart
+from src.service.price import get_historical_price
 
 router = Router()
 
 
 @router.message()
 async def all_msg_handler(message: Message):
-    await send_chart(bot=message.bot, user_id=message.from_user.id, interval=LABEL_1M)
+    await message.delete()
+    await send_chart(bot=message.bot, user_id=message.from_user.id)
 
 
 @router.callback_query()
@@ -20,15 +22,14 @@ async def all_cb_handler(callback: CallbackQuery):
     await callback.answer()
 
 
-async def send_chart(user_id, bot: Bot, interval, msg_id=None):
-    charts_data = {LABEL_TIME: {'chart_type': CT_LINE, 'candle_time': None},
-                   LABEL_1M: {'chart_type': CT_CANDLESTICK, 'candle_time': '1T'},
-                   LABEL_15M: {'chart_type': CT_CANDLESTICK, 'candle_time': '15T'},
-                   LABEL_1H: {'chart_type': CT_CANDLESTICK, 'candle_time': '1H'}}
+async def send_chart(user_id, bot: Bot, interval: str = LABEL_1H, msg_id=None):
+    period = {LABEL_1M: '1d', LABEL_15M: '2d', LABEL_1H: '3d'}[interval]
+    hist_df = await get_historical_price(period=period, interval=interval)
 
-    chart_data = charts_data[interval]
-    chart_img_bytes, last_price = make_chart(chart_type=chart_data['chart_type'], candle_time=chart_data['candle_time'])
+    chart_img_bytes = await make_chart(df=hist_df)
     chart_img = BufferedInputFile(chart_img_bytes, filename="chart.png")
+
+    last_price = round(hist_df.tail(1)['Close'].item(), 2)
     msg_txt = f"{last_price}$ - 1₿"
 
     config = load_config()
@@ -37,6 +38,5 @@ async def send_chart(user_id, bot: Bot, interval, msg_id=None):
     if msg_id is None:
         await bot.send_photo(chat_id=user_id, photo=chart_img, caption=msg_txt, reply_markup=kb)
     else:
-
         await bot.edit_message_media(chat_id=user_id, message_id=msg_id, reply_markup=kb,
                                      media=InputMediaPhoto(media=chart_img, caption=msg_txt))
