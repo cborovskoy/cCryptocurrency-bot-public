657b403c231a45123c7cfa75bbfd49e447f5bfe6
Constantine <cborovskoy@gmail.com>
2023-08-04 01:29:59 +0600

    Update main_kb.py

--- src/keyboards/main_kb.py
+++ src/keyboards/main_kb.py
@@ -1,15 +1,14 @@
 from aiogram.types import InlineKeyboardMarkup
 from aiogram.utils.keyboard import InlineKeyboardBuilder, InlineKeyboardButton
 
-from src.config import LABEL_TIME, LABEL_1M, LABEL_15M, LABEL_1H
+from src.config import LABEL_15M, LABEL_1H, LABEL_1M
 
 
 def get_main_kb(coinbase_invite: str, binance_invite: str) -> InlineKeyboardMarkup:
     builder = InlineKeyboardBuilder()
-    builder.row(InlineKeyboardButton(text=LABEL_TIME, callback_data=LABEL_TIME),
-                InlineKeyboardButton(text=LABEL_1M, callback_data=LABEL_1M),
-                InlineKeyboardButton(text=LABEL_15M, callback_data=LABEL_15M),
-                InlineKeyboardButton(text=LABEL_1H, callback_data=LABEL_1H))
+    builder.row(InlineKeyboardButton(text='1m [1d]', callback_data=LABEL_1M),
+                InlineKeyboardButton(text='15m [2d]', callback_data=LABEL_15M),
+                InlineKeyboardButton(text='1h [3d]', callback_data=LABEL_1H))
     builder.row(InlineKeyboardButton(text='open BINANCE', url=binance_invite),
                 InlineKeyboardButton(text='open Coinbase', url=coinbase_invite))
-    return builder.as_markup(resize_keyboard=True)
+    return builder.as_markup()
