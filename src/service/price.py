da1b7519859991466cb156fc9e25d273bbd6840d
Constantine <cborovskoy@gmail.com>
2023-08-20 23:01:47 +0600

    Add files via upload

--- src/service/price.py
+++ src/service/price.py
@@ -1,10 +1,11 @@
+import datetime
 import logging
 import yfinance as yf
 from requests import Session
 from requests_cache import CacheMixin, SQLiteCache
 from requests_ratelimiter import LimiterMixin, MemoryQueueBucket
 from pyrate_limiter import Duration, RequestRate, Limiter
-
+import pandas as pd
 
 logger = logging.getLogger(__name__)
 
@@ -13,7 +14,7 @@ class CachedLimiterSession(CacheMixin, LimiterMixin, Session):
     pass
 
 
-async def get_historical_price(period: str, interval: str):
+async def get_historical_price(period_in_days: int, interval: str) -> pd.DataFrame:
     session = CachedLimiterSession(
         limiter=Limiter(RequestRate(1, Duration.SECOND * 2)),  # max 1 requests per 2 seconds
         bucket_class=MemoryQueueBucket,
@@ -21,6 +22,8 @@ async def get_historical_price(period: str, interval: str):
     )
 
     btc_usd = yf.Ticker("BTC-USD", session=session)
-    hist = btc_usd.history(period=period, interval=interval)
-    return hist
 
+    end_date = datetime.datetime.utcnow().date() + datetime.timedelta(days=1)
+    start_date = end_date - datetime.timedelta(days=period_in_days)
+
+    return btc_usd.history(start=start_date, end=end_date, interval=interval)
